19a4715f26e97f3fdf68985aae51fc042ce7c96f
molodec08 <molodec.08@mail.ru>
2022-09-25 17:23:23 +0300

    add eslint, fix all files

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,27 +1,23 @@
-import React, {Suspense} from 'react';
-import {Route, Routes} from "react-router-dom";
-import {AboutPage} from "pages/AboutPage";
-import {MainPage} from "pages/MainPage";
-import {RouterConfig} from "shared/config/routeConfig/routeConfig";
+import React, { Suspense } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { RouterConfig } from 'shared/config/routeConfig/routeConfig';
 
-const AppRouter = () => {
-  return (
-    <Routes>
-      {Object.values(RouterConfig).map(({path, element}) => (
-        <Route
-          key={path}
-          path={path}
-          element={(
-            <Suspense fallback={<div>Loading...</div>}>
-              <div className="page-wrapper">
-                {element}
-              </div>
-            </Suspense>
-          )}
-        />
-      ))}
-    </Routes>
-  );
-};
+const AppRouter = () => (
+  <Routes>
+    {Object.values(RouterConfig).map(({ path, element }) => (
+      <Route
+        key={path}
+        path={path}
+        element={(
+          <Suspense fallback={<div>Loading...</div>}>
+            <div className="page-wrapper">
+              {element}
+            </div>
+          </Suspense>
+        )}
+      />
+    ))}
+  </Routes>
+);
 
 export default AppRouter;
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -1,26 +1,27 @@
-import {classNames} from "shared/lib/classNames/classNames";
+import { classNames } from 'shared/lib/classNames/classNames';
+import { Button } from 'shared/ui/Button/Button';
+import { useState } from 'react';
+import { ThemeSwitcher } from 'widgets/ThemeSwitcher';
+import { LangSwitcher } from 'widgets/LangSwitcher/LangSwitcher';
 import cls from './Sidebar.module.scss';
-import {useState} from "react";
-import {ThemeSwitcher} from "widgets/ThemeSwitcher";
-import {LangSwitcher} from "widgets/LangSwitcher/LangSwitcher";
 
 interface SidebarProps {
   className?: string;
 }
 
-export const Sidebar = ({className}: SidebarProps) => {
-  const [collapsed, setCollapsed] = useState(false)
+export const Sidebar = ({ className }: SidebarProps) => {
+  const [collapsed, setCollapsed] = useState(false);
 
-  const onToggle = () => setCollapsed(prev => !prev)
+  const onToggle = () => setCollapsed((prev) => !prev);
 
   return (
     <div
-      className={classNames(cls.Sidebar, {[cls.collapsed]: collapsed}, [className])}
+      className={classNames(cls.Sidebar, { [cls.collapsed]: collapsed }, [className])}
     >
-      <button onClick={onToggle}>toggle</button>
+      <Button onClick={onToggle}>toggle</Button>
       <div className={cls.switchers}>
         <ThemeSwitcher />
-        <LangSwitcher className={cls.lang}/>
+        <LangSwitcher className={cls.lang} />
       </div>
     </div>
   );
