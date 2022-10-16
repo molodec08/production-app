7edc227a9efa244af07b5c96cf047bcc5e46cfb9
molodec08 <molodec.08@mail.ru>
2022-10-16 18:23:34 +0300

    add modal, portal, change header, gitignore

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,6 +22,7 @@ module.exports = {
     'react',
     '@typescript-eslint',
     'i18next',
+    'react-hooks',
   ],
   rules: {
     'react/jsx-indent': [2, 2],
@@ -49,14 +50,20 @@ module.exports = {
       ignoreComments: true,
       code: 100,
     }],
+    'jsx-a11y/no-static-element-interactions': 'off',
+    'jsx-a11y/click-events-have-key-events': 'off',
+    'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
+    'react-hooks/exhaustive-deps': 'error', // Checks effect dependencies,
+    'no-param-reassign': 'off',
   },
   globals: {
     __IS_DEV__: true,
   },
   overrides: [{
-    files: ['**/src/**/*.test.{ts,tsx}'],
+    files: ['**/src/**/*.{test,stories}.{ts,tsx}'],
     rules: {
       'i18next/no-literal-string': 'off',
+      'max-len': 'off',
     },
   }],
 };
