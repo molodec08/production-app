47068fb0dcabb33db0788b7f3f94225f0a4b657b
molodec08 <molodec.08@mail.ru>
2023-01-05 15:12:42 +0300

    change storybook setting, pre-commit, ci settings, fix bugs

--- src/shared/ui/Popups/ui/ListBox/ListBox.tsx
+++ src/shared/ui/Popups/ui/ListBox/ListBox.tsx
@@ -3,7 +3,6 @@ import { Listbox as HListBox } from '@headlessui/react';
 import { classNames } from '@/shared/lib/classNames/classNames';
 import { DropdownDirection } from '@/shared/types/ui';
 import { HStack } from '../../../Stack';
-import { Button } from '../../../Button/Button';
 import cls from './ListBox.module.scss';
 import { mapDirectionClass } from '../../styles/consts';
 import popupCls from '../../styles/popup.module.scss';
@@ -50,9 +49,7 @@ export function ListBox(props: ListBoxProps) {
         onChange={onChange}
       >
         <HListBox.Button disabled={readonly} className={cls.trigger}>
-          <Button disabled={readonly}>
-            {value ?? defaultValue}
-          </Button>
+          {value ?? defaultValue}
         </HListBox.Button>
         <HListBox.Options className={classNames(cls.options, {}, optionsClasses)}>
           {items?.map((item) => (
